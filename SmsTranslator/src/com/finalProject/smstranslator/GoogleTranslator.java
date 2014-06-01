cac802855e885f1b55916cff6f4955550ad4f534
Avi <avi.turner111@gmail.com>
2014-06-01 22:39:31 +0300

    Translation enhancements.
    Translation works in another Thread.
    For now just English to Hebrew.

--- SmsTranslator/src/com/finalProject/smstranslator/GoogleTranslator.java
+++ SmsTranslator/src/com/finalProject/smstranslator/GoogleTranslator.java
@@ -2,10 +2,12 @@ package com.finalProject.smstranslator;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
 
 public class GoogleTranslator {
 
@@ -13,8 +15,8 @@ public class GoogleTranslator {
 	/** 
 	 The template for translation requests from google 
 	 */
-	final static String GOOGLE_TRANSLATE_URL_TEMPLATE = "http://translate.google.com/translate_a/t?client=p&hl=%s&sl=%s&tl=%s&ie=UTF-8&oe=UTF-8&multires=1&oc=2&otf=1&ssel=0&tsel=0&pc=1&sc=1&q=%s";
-
+	final static String GOOGLE_TRANSLATE_URL_TEMPLATE = //"http://translate.google.com/translate_a/t?client=p&hl=%s&sl=%s&tl=%s&ie=UTF-8&oe=UTF-8&multires=1&oc=2&otf=1&ssel=0&tsel=0&pc=1&sc=1&q=%s";
+	"http://translate.google.com.tw/translate_a/t?client=t&hl=en&sl=%s&tl=%s&ie=UTF-8&oe=UTF-8&multires=1&oc=1&otf=2&ssel=0&tsel=0&sc=1&q=%s";
 
 	//"http://translate.google.com/translate_a/t?client=t&hl=iw&sl=auto&tl=iw&ie=UTF-8&oe=UTF-8&multires=1&oc=1&prev=conf&psl=en&ptl=iw&otf=1&it=sel.8772&ssel=3&tsel=6&uptl=iw&alttl=en&sc=1&q=dog"
 
@@ -40,12 +42,14 @@ public class GoogleTranslator {
 
 		String urlStr = GoogleTranslator.GetTranslateUrl(expression, languageFrom, languageTo);
 		URL url = new URL(urlStr);
-		/*URL url = new URL("http://translate.google.com.tw/translate_a/t?client=t&hl=en&sl=" +
-                sl + "&tl=" + tl + "&ie=UTF-8&oe=UTF-8&multires=1&oc=1&otf=2&ssel=0&tsel=0&sc=1&q=" + 
-                URLEncoder.encode(text, "UTF-8"));*/
+		
 		URLConnection urlConnection = url.openConnection();
 		urlConnection.setRequestProperty("User-Agent", "Something Else");
-		BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+		
+		InputStream inputStream = urlConnection.getInputStream();
+		InputStreamReader inputreader= new InputStreamReader(inputStream);
+		BufferedReader br = new BufferedReader(inputreader);
+		
 		String result = br.readLine();
 		br.close();
 
@@ -92,11 +96,9 @@ Gets the translate URL.
 
 		String targetSymbol = languageTo.getSymbol();
 		String sourceSymbol = languageFrom.getSymbol();
-*/
-
-		expression = "hello world!";
-		String sourceSymbol = "he";
-		String targetSymbol = "en";
+*/		
+		String sourceSymbol = "en";
+		String targetSymbol = "he";
 		
 		return GetUrlBySymbols(expression, sourceSymbol, targetSymbol);
 	}
@@ -114,13 +116,15 @@ Gets the URL by symbols.
 	private static String GetUrlBySymbols(String expression, String sourceSymbol, String targetSymbol) throws URISyntaxException, IOException
 	{
 		// for a parameter called SL in the URL...
-		String slParam = sourceSymbol == null || sourceSymbol.trim() == "" ? "auto" : sourceSymbol;
+		//String slParam = sourceSymbol == null || sourceSymbol.trim() == "" ? "auto" : sourceSymbol;
 
 		//this is for passing the expression in encodeURIcomponent format for non English expressions
 		//String encodedSentene = Uri.EscapeUriString(expression);
 		
 		
-		String url = String.format(GoogleTranslator.GOOGLE_TRANSLATE_URL_TEMPLATE, sourceSymbol, slParam, targetSymbol, expression);
+		//String url = String.format(GoogleTranslator.GOOGLE_TRANSLATE_URL_TEMPLATE, sourceSymbol, slParam, targetSymbol, expression);
+		String encodedExpression = URLEncoder.encode(expression, "UTF-8");
+		String url = String.format(GoogleTranslator.GOOGLE_TRANSLATE_URL_TEMPLATE, sourceSymbol, targetSymbol, encodedExpression);
 		//URL tempURL = new URL(url);
 		
 		//String encodedUrl = tempURL.toURI().toString();		
