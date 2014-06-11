36b11cab4403b9b3d1d13f91d8ec72e7123ff080
Ben Ohana <ben.ohana1@gmail.com>
2014-06-11 09:23:58 +0300

    add background image

--- SmsTranslator/src/com/finalProject/smstranslator/ConversationActivity.java
+++ SmsTranslator/src/com/finalProject/smstranslator/ConversationActivity.java
@@ -19,26 +19,26 @@ public class ConversationActivity extends Activity implements IOnTranslationComp
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_conversation);
-		
+
 		Bundle extra =  getIntent().getExtras();
 		String address = extra.getString("address");
-		
+
 		SMSProvider provider = new SMSProvider(this);
 		SMSAdapter adapter = new SMSAdapter(this, provider.getSMSDetails(address));
-				
+
 		ListView lv = (ListView) findViewById(R.id.conversationList);
 		lv.setAdapter(adapter);
 		lv.setSelection(adapter.getCount() - 1);
-		
+
 		TextView add = (TextView) findViewById(R.id.tv_address);
-		
+
 		ContactHelper ch = new ContactHelper(this);
 		String name = ch.getContactName(address);
 		if(name == null)
 			add.setText(address);
 		else
 			add.setText(name);
-			
+
 	}
 
 	@Override
@@ -47,7 +47,7 @@ public class ConversationActivity extends Activity implements IOnTranslationComp
 		getMenuInflater().inflate(R.menu.conversation, menu);
 		return true;
 	}
-	
+
 	public void onTranslationCompleted(String result)
 	{
 		if (this._translatedView.getId() == R.id.btnSend) {
@@ -56,39 +56,39 @@ public class ConversationActivity extends Activity implements IOnTranslationComp
 		else if (this._translatedView instanceof EditText) {
 			EditText txb = (EditText) this._translatedView;
 			txb.setText(result);
-		   
+
 		}
 		else if (this._translatedView instanceof TextView) {
-		    TextView textView = (TextView) this._translatedView;
-		    textView.setText(result);
+			TextView textView = (TextView) this._translatedView;
+			textView.setText(result);
 		}
 	}
-	
+
 	public void translate(View view){
-		
+
 		EditText txbMessage = (EditText) findViewById(R.id.txbMessage);
 		String expression = txbMessage.getText().toString();
 		this.triggerTranslation(txbMessage, expression);
-		
-		
+
+
 	}
 
-	
-	
+
+
 	public void sendSMS(View view){
 		ImageButton btnSend = (ImageButton) findViewById(R.id.btnSend);
 		EditText txbMessage = (EditText) findViewById(R.id.txbMessage);
 		String expression = txbMessage.getText().toString();
 		this.triggerTranslation(btnSend, expression);
 	}
-	
-private void triggerTranslation(View activeControl, String expression) {	
-		
+
+	private void triggerTranslation(View activeControl, String expression) {	
+
 		AsyncTranslator asyncTrnaslator = new AsyncTranslator(); 
-		
+
 		String symbolFrom = PreferencesManager.getLanguageFrom();
 		String symbolTo = PreferencesManager.getLanguageTo();
-		
+
 		this._translatedView = activeControl;
 		asyncTrnaslator.execute(expression,symbolFrom, symbolTo,this);
 	}
